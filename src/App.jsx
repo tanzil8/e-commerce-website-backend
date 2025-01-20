391c8ae833bdffe1817d9c03b2102807387501c3
TasmiyaKhan8028 <155808841+TasmiyaKhan8028@users.noreply.github.com>
2025-01-20 14:07:54 -0800

    home page com

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ResponsiveNav from './componenets/navbar'
 import Banner from './componenets/banner'
+import FlashProduct from './componenets/flashProduct'
+import Categories from './componenets/categories'
+import ProductShowcase from './componenets/product'
+import Fooyer from './componenets/footer'
 
 
 const App = () => {
@@ -8,6 +12,10 @@ const App = () => {
     <>
     <ResponsiveNav/>
     <Banner/>
+    <FlashProduct/>
+    <Categories/>
+    <ProductShowcase/>
+    <Fooyer/>
       </>
   )
 }
